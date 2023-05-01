570be6ff2444cf395e0fddbe78804d8f412b1b91
vasiliy <vash050@rmbler.ru>
2023-05-01 20:38:57 +0300

    new template

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return render_template('index1.html')
+def index():  # put application's code here
+    return render_template('index.html')
+
+
+@app.route('/gallery/')
+def gallery():  # put application's code here
+    return render_template('gallery.html')
 
 
 if __name__ == '__main__':

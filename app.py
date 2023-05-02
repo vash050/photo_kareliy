4c463b404011c6ab4af39f9d9a3fb22187cd4f74
vasiliy <vash050@rmbler.ru>
2023-05-02 17:13:33 +0300

    clear files+

--- app.py
+++ app.py
@@ -8,10 +8,5 @@ def index():  # put application's code here
     return render_template('index.html')
 
 
-@app.route('/gallery/')
-def gallery():  # put application's code here
-    return render_template('gallery.html')
-
-
 if __name__ == '__main__':
     app.run()

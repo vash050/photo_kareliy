216ae6aef56aa26e5ecb409d6ed3a20b057dfb86
vasiliy <vash050@rmbler.ru>
2023-12-16 11:01:09 +0300

    - burger + old

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
+import os
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'sdfjkhjsdnsnk32i3jbruyfk_wurj**msfj'
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index():  # put application's code here
-    return render_template('index.html')
+    return render_template('main_block.html')
 
 
 if __name__ == '__main__':
